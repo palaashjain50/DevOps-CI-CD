d1c9d333e7ea0ebef66e1f53b08cb12b42037a75
palaashjain50 <124269943+palaashjain50@users.noreply.github.com>
2023-11-26 11:05:53 +0530

    First Commit

---
+++ src/index.test.js
@@ -0,0 +1,28 @@
+// import files
+var add = require('./calculator/add')
+var subtract = require('./calculator/subtract')
+var multiply = require('./calculator/multiply')
+var divide = require('./calculator/divide')
+
+
+// Test Case 1 (Addition)
+test("Should add two numbers", () => {
+  expect(add(25, 10)).toBe(35);
+});
+
+test("Should divide two numbers", () => {
+  expect(divide(25, 5)).toBe(5);
+});
+
+test("Should multiply two numbers", () => {
+  expect(multiply(25, 10)).toBe(250);
+});
+
+// Test Case 2 (Subtraction)
+test("Should subtract two numbers", () => {
+  expect(subtract(25, 10)).toBe(15);
+});
+
+// Test Case 3 (Multiplication)
+
+// Test Case 4 (Division)
